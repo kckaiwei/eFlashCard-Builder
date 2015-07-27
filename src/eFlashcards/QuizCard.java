31b5522c7e5b35b713f9bc5df4adab428d5e7c68
Kevin Chang <kckaiwei@gmail.com>
2015-07-27 13:45:25 -0500

    eFlashCard Creator and Retriever
    creates and fetches digital flashcards

---
+++ src/eFlashcards/QuizCard.java
@@ -0,0 +1,20 @@
+package eFlashcards;
+
+public class QuizCard {
+	private String question;
+	private String answer;
+	
+	public QuizCard(String q, String a) {
+		// TODO Auto-generated constructor stub
+		question = q;
+		answer = a;
+	}
+
+	public String getQuestion() {
+		return question;
+	}
+	
+	public String getAnswer() {
+		return answer;
+	}
+}
